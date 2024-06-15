ff14c0b5c842c70c4c255270fcc39726c9466f40
My name <me@example.com>
2024-06-15 18:36:17 +0530

    create a main function for main

--- disk_usage.py
+++ disk_usage.py
@@ -12,7 +12,9 @@ def check_disk_usage(disk, min_absolute, min_percent):
     if percent_free < min_percent or gigabytes_free < min_absolute:
         return False
     return True
-
+def check_root_full():
+    """Returns true if the root position is full,false otherwise"""
+    return check_disk_full(disk="/",min_gb=2,min_percent=10)
 # Check for at least 2 GB and 10% free
 if not check_disk_usage("/", 2*2**30, 10):
     print("ERROR: Not enough disk space")
@@ -20,3 +22,10 @@ if not check_disk_usage("/", 2*2**30, 10):
 
 print("Everything is  okay")
 return 0
+def main():
+    if check_reboot():
+        print("Pending Reboot.")
+        sys.exit(1)
+    if check_root_full():
+        print("Root partition full.")
+        sys.exit(1)
